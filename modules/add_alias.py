a58e09751da5d23b81e67551c7c174355fe724a9
Filipe Figueiredo <70453488+Filz0r@users.noreply.github.com>
2021-01-02 21:54:38 +0000

    Update add_alias.py

--- modules/add_alias.py
+++ modules/add_alias.py
@@ -3,8 +3,7 @@ import os
 # you can change os.system() for subprocess.run() it should still work
 
 
-def add_alias():
-    print('''
+print('''
 ##############################
 ##  
 ##  This script adds armyknife alias to run this script in the end of the
@@ -12,18 +11,33 @@ def add_alias():
 ##  IT ASSUMES YOU HAVE THE SCRIPT FOLDER IN YOUR HOME DIRECTORY
 ##
 ##############################''')
-    try:
-        username = input('What is the username on your system?: (case sensitive)')
-        print(username)
-        command = "echo alias armyknife='/home/%d/My-Linux-Swiss-Army-Knife/myLinuxSwissArmyKnife.py' >> /home/%d/.bashrc" % username
-        print(command)
-        os.system(command)
-    except:
-        print('you dumb fucker')
-        print(username)
-        print(command)
-        return add_alias()
-    
-    # required part for the script to return to the menu after running its job
-    from lib.category_menu import menu
-    return menu()
+
+username = input('What is the username on your system?: (case sensitive)')
+command = "echo alias armyknife='/home/{}/My-Linux-Swiss-Army-Knife/myLinuxSwissArmyKnife.py' >> /home/{}/.bashrc".format(
+    username, username)
+print(username)
+print(command)
+os.system(command)
+
+# def add_alias():
+#    print('''
+##############################
+##
+# This script adds armyknife alias to run this script in the end of the
+# .bashrc file of the user that ran it
+# IT ASSUMES YOU HAVE THE SCRIPT FOLDER IN YOUR HOME DIRECTORY
+##
+# ''')
+#    try:
+#        username = input('What is the username on your system?: (case sensitive)')
+#        command = "{echo alias armyknife='/home/{}/My-Linux-Swiss-Army-Knife/myLinuxSwissArmyKnife.py' >> /home/{}/.bashrc}".format(username)
+#        os.system(command)
+#    except:
+#        print('you dumb fucker')
+#        print(username)
+#        print(command)
+#        return add_alias()
+
+# required part for the script to return to the menu after running its job
+#    from lib.category_menu import menu
+#    return menu()
